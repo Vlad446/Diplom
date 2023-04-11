dc87b671b40adafc85d3174f4648f35b92cbb2ad
Vlad446 <80104334+Vlad446@users.noreply.github.com>
2023-04-12 03:29:47 +0500

    2

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        //диаметр сечения для образцов круглого сечения
+        public double d;
+        //длины сторон для образцов прямоугольного сечения
+        public double a0;
+        public double bo;
+        //начальная расчетная длина
+        public double l0;
+        //рабочая длина
+        public double dlina_raboch;
+        //конечная длина
+        public double lk;
+        //конечный диаметр сечения
+        public double d_ko;
+
+        //относительное удлинение
+        public double Otn_udlin(double l0, double lk)
+        {
+            return Math.Round(100 * (lk - l0) / l0, 4);
+        }
+    }
+}
--- Razryvnaya_machine_VKR/Form1.cs
+++ Razryvnaya_machine_VKR/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,28 @@ namespace Razryvnaya_machine_VKR
         public Form1()
         {
             InitializeComponent();
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Считывание значения исходных данных
+            double d = double.Parse(textBox1.Text);
+            double a0 = double.Parse(textBox2.Text);
+            double bo = double.Parse(textBox3.Text);
+            double l0 = double.Parse(textBox4.Text);
+            double dlina_raboch = double.Parse(textBox5.Text);
+            double lk = double.Parse(textBox6.Text);
+            double d_ko = double.Parse(textBox7.Text);
+
+            // Вывод значения в окно формулы
+
+            // Вычисляем арифметическое выражение
+            double Otn_udlin = 100 * (lk - l0) / l0;
+
+            // Выводим результат в окно
+            textBox11.Text += Environment.NewLine+ Otn_udlin.ToString();
         }
     }
 }
